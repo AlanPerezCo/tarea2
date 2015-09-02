e92505449a6e99473138e0de046cb89724d45005
Alan Pérez Castillo <alan.perezco@udlap.mx>
2015-09-02 01:39:50 -0500

    Segunda tarea

--- sherlock.js
+++ sherlock.js
@@ -22,3 +22,40 @@ _INPUT = "2\n3 9\n17 24";
 */
 
 //----- Start your code here -------
+Dates1=_INPUT.split("\n");//Quit the value"\n"
+Size=parseInt(Dates1.shift(),10);//Get the first value
+Dates2=_INPUT.split(" ");//Quit the value " "
+Changer=0;
+Accumulator=0;
+if(1<=Size&&Size<=100){//Contional about size
+	console.log("Waston says:\n"+"size: "+Size);
+}
+else{
+	console.log("Waston says:\n"+"size: "+Size);
+	console.log("Sherlock answer: Your size aren't accepted");//if the size isn't accepted, end program
+	process.exit();
+}
+for(i=0;i<Size;i++){
+	RangeS=parseInt(Dates1[0+i],10);//Obtain the start of range
+	RangeF=parseInt(Dates2[1+i],10);//Obtain the finish of range
+	if(RangeS>RangeF){//changing the order if the start of range is minor than the finish range
+		Changer=RangeS;
+		RangeS=RangeF;
+		RangeF=Changer;
+	}
+	console.log((i+1)+".- "+RangeS+" ~ "+RangeF);//printing the start and finish range
+	if(1<=RangeS&&RangeF<=Math.pow(10,9)){//Conditional about range
+		for(j=RangeS;j<=RangeF;j++){
+			if(Math.sqrt(j)%1==0){
+				Accumulator++;//Plus the perfect squares
+			}
+		}
+		console.log("Sherlock answer:");
+		console.log(Accumulator+" perfect square"+"\n");
+		Accumulator=0;//Changing value to 0 for the next case
+	}
+	else{		
+		console.log("Sherlock answer: Your range aren't accepted");//if the range ins't accepted, end program
+		process.exit();
+	}	
+}
